f567e0fee2ef918d9a76db38c8f3437ff25bf82d
Juncen Li <li.junce@norhteastern.edu>
2022-10-26 17:01:22 -0700

    EncodeUserPassword

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.shopme.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// 这个文件是配置文件，到时候会被自动扫描，我们在这个配置文件中，permit了所有的请求，所以不需要输入密码
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 和UserService里面的 - > private PasswordEncoder passwordEncoder 相互链接
+    // PasswordEncoder is abstract, we use BCryptPasswordEncoder to instantiate PasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
+}
