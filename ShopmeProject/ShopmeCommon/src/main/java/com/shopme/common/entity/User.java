f4566e11f836036259e087d0db03944955c650ce
Juncen Li <li.junce@norhteastern.edu>
2022-11-19 15:18:17 -0800

    UpdateUsersListingForMobile

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -138,5 +138,10 @@ public class User {
         return "/user-photos/" + this.id + "/" + this.photos;
     }
 
+    @Transient
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
 
 }
