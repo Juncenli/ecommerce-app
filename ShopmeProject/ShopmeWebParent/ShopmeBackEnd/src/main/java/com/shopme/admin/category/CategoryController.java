5cc3f88bbc2385c33949db9229ca99c88e6fd776
Juncen Li <li.junce@norhteastern.edu>
2022-12-06 12:11:51 -0500

    CodeUpdateCategoryFunction

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -1,20 +1,20 @@
 package com.shopme.admin.category;
 
-import java.io.IOException;
-import java.util.List;
-
 import com.shopme.admin.FileUploadUtil;
+import com.shopme.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.shopme.common.entity.Category;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class CategoryController {
@@ -44,14 +44,38 @@ public class CategoryController {
     public String saveCategory(Category category,
                                @RequestParam("fileImage") MultipartFile multipartFile,
                                RedirectAttributes ra) throws IOException {
-        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-        category.setImage(fileName);
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            category.setImage(fileName);
 
-        Category savedCategory = service.save(category);
-        String uploadDir = "../category-images/" + savedCategory.getId();
-        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+            Category savedCategory = service.save(category);
+            String uploadDir = "../category-images/" + savedCategory.getId();
+
+            FileUploadUtil.cleanDir(uploadDir);
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        } else {
+            service.save(category);
+        }
 
         ra.addFlashAttribute("message", "The category has been saved successfully.");
         return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/edit/{id}")
+    public String editCategory(@PathVariable(name = "id") Integer id, Model model,
+                               RedirectAttributes ra) {
+        try {
+            Category category = service.get(id);
+            List<Category> listCategories = service.listCategoriesUsedInForm();
+
+            model.addAttribute("category", category);
+            model.addAttribute("listCategories", listCategories);
+            model.addAttribute("pageTitle", "Edit Category (ID: " + id + ")");
+
+            return "categories/category_form";
+        } catch (CategoryNotFoundException ex) {
+            ra.addFlashAttribute("message", ex.getMessage());
+            return "redirect:/categories";
+        }
     }
 }
