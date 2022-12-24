5ab7b5981d05ae831841b33ffe6bb2e1e8560c15
Juncen Li <li.junce@norhteastern.edu>
2022-12-24 15:41:29 -0500

    CodeProductDetailEntityClass

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -1,9 +1,7 @@
 package com.shopme.common.entity;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "products")
@@ -81,6 +79,9 @@ public class Product {
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     private Set<ProductImage> images = new HashSet<>();
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductDetail> details = new ArrayList<>();
 
     public Integer getId() {
         return id;
@@ -261,6 +262,19 @@ public class Product {
         if (id == null || mainImage == null) return "/images/image-thumbnail.png";
 
         return "/product-images/" + this.id + "/" + this.mainImage;
+    }
+
+
+    public List<ProductDetail> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<ProductDetail> details) {
+        this.details = details;
+    }
+
+    public void addDetail(String name, String value) {
+        this.details.add(new ProductDetail(name, value, this));
     }
 
 }
