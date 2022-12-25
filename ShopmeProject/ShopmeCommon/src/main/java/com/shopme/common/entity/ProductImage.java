459bdb64d75d5d2ca15de5945c45ab26b276b752
Juncen Li <li.junce@norhteastern.edu>
2022-12-25 13:46:03 -0500

    CodeEditProductFunction

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/ProductImage.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/ProductImage.java
@@ -1,6 +1,14 @@
 package com.shopme.common.entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "product_images")
@@ -49,5 +57,9 @@ public class ProductImage {
         this.product = product;
     }
 
+    @Transient
+    public String getImagePath() {
+        return "/product-images/" + product.getId() + "/extras/" + this.name;
+    }
 
 }
